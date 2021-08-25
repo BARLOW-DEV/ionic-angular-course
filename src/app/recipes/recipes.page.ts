de37090e705fa4f5ba84bd3718a1bc3fc1d5c841
Aaron Barlow <abarlow@hydrotexlube.com>
2021-08-25 14:48:33 -0500

    Initial commit

---
+++ src/app/recipes/recipes.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.page.html',
+  styleUrls: ['./recipes.page.scss'],
+})
+export class RecipesPage implements OnInit {
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/440px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+      ingredients: ['French Fries', 'Pork Meat', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Spaghetti_bolognese_%28hozinja%29.jpg/440px-Spaghetti_bolognese_%28hozinja%29.jpg',
+      ingredients: ['noodles', 'Meat', 'sauce']
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
